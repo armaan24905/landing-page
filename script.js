ea7da94d795596b9d814b65f63a2d310ed751211
Armaan Saad <armaanvnb007@gmail.com>
2026-03-06 22:48:22 +0530

    Updates
    Some little changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Smooth scroll to ABOUT section
+function scrollToAbout() {
+const section = document.querySelector("#about");
+section.scrollIntoView({
+behavior: "smooth"
+});
+}
+// Smooth scroll to PROJECTS section
 function scrollToProjects() {
 const section = document.querySelector("#projects");
 section.scrollIntoView({
@@ -8,3 +16,15 @@ setTimeout(() => {
 section.classList.remove("highlight");
 }, 2000);
 }
+// Scroll animation
+const observer = new IntersectionObserver((entries) => {
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+document.querySelectorAll("section").forEach(section => {
+section.classList.add("hidden");
+observer.observe(section);
+});
